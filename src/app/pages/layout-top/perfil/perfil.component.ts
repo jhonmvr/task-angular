8a3f25b21a91c575633aa8148eb80247fc05018f
Lenin Yaguachi <leninyaguachi@gmail.com>
2023-06-20 15:05:23 -0500

    icono de perfil

--- src/app/pages/layout-top/perfil/perfil.component.ts
+++ src/app/pages/layout-top/perfil/perfil.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+interface User{
+  name:string,
+  id:number
+}
+
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
   styleUrls: ['./perfil.component.css']
 })
 export class PerfilComponent {
+  user: User; 
+  isMenuOpen: boolean = false;
+  constructor() {
+    this.user = { name: 'Juan Perez', id: 1};
+  }
 
+  ngOnInit() { }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
